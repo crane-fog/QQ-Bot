97170c1c2ecb8cb87bb53066b22e78fdcd7869d0
Heai <13901908918@163.com>
2025-12-21 21:39:48 +0800

    fix: Theresac

--- Plugins/Theresac/Theresac.py
+++ Plugins/Theresac/Theresac.py
@@ -14,16 +14,16 @@ class Theresac(Plugins):
                             """
         self.init_status()
 
-    @plugin_main(call_word=["Theresac"])
+    @plugin_main(check_group=False, call_word=["Theresac"])
     async def main(self, event: GroupMessageEventHandler, debug):
         message = event.message
 
         if not event.user_id == self.bot.owner_id:
             return
 
-        cmd = message.split(" ")[1:]
+        cmd = " ".join(message.split(" ")[1:])
         result = subprocess.run(cmd, capture_output=True, text=True, shell=True)
 
-        msg = result.stdout
+        msg = result.stdout.replace("[CQ:", "\\CQ:").strip()
         self.api.GroupService.send_group_msg(self, group_id=event.group_id, message=f"{msg}")
         return
