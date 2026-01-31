d51842fb7e8ad497d7671818637f54c846288270
Heai <13901908918@163.com>
2026-02-01 00:36:39 +0800

    feat: record message to database

---
+++ Plugins/MessageRecorder/MessageRecorder.py
@@ -0,0 +1,41 @@
+from sqlalchemy import Column, BigInteger, Text, DateTime, func
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from Plugins import plugin_main, Plugins
+from Event.EventHandler.GroupMessageEventHandler import GroupMessageEvent
+
+Base = declarative_base()
+
+class Message(Base):
+    __tablename__ = 'messages'
+    
+    id = Column(BigInteger, primary_key=True, autoincrement=True)
+    user_id = Column(BigInteger, nullable=False)
+    group_id = Column(BigInteger, nullable=False)
+    msg = Column(Text)
+    send_time = Column(DateTime(timezone=True), nullable=False, server_default=func.now())
+
+class MessageRecorder(Plugins):
+    def __init__(self, server_address, bot):
+        super().__init__(server_address, bot)
+        self.name = "MessageRecorder"
+        self.type = "Group"
+        self.author = "Heai"
+        self.introduction = """
+                                记录群聊消息到数据库
+                                usage: auto
+                            """
+        self.init_status()
+
+    @plugin_main(check_call_word=False, check_group=False, require_db=True)
+    async def main(self, event: GroupMessageEvent, debug):
+        async_sessions = sessionmaker(bind=self.bot.database, class_=AsyncSession, expire_on_commit=False)
+        async with async_sessions() as session:
+            async with session.begin():
+                new_msg = Message(
+                    user_id=event.user_id,
+                    group_id=event.group_id,
+                    msg=event.message
+                )
+                session.add(new_msg)
---
+++ Plugins/MessageRecorder/__init__.py
@@ -0,0 +1,1 @@
+from .MessageRecorder import MessageRecorder
