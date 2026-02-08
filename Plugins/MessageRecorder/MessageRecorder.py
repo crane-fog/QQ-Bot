259cda3bb60bef2566b6016d0c643af46b92415f
Heai <13901908918@163.com>
2026-02-08 19:58:16 +0800

    feat: use sql to provide chat context

--- Plugins/MessageRecorder/MessageRecorder.py
+++ Plugins/MessageRecorder/MessageRecorder.py
@@ -1,12 +1,18 @@
-from sqlalchemy import BigInteger, Column, DateTime, Text, func
+import re
+
+from sqlalchemy import BigInteger, Column, DateTime, Text, func, select
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 from Event.EventHandler.GroupMessageEventHandler import GroupMessageEvent
+from Logging.PrintLog import Log
 from Plugins import Plugins, plugin_main
 
+log = Log()
 Base = declarative_base()
+
+PATTERN = re.compile(r"\[CQ:reply,id=(-?\d+)\]")
 
 
 class Message(Base):
@@ -34,6 +40,21 @@ class MessageRecorder(Plugins):
                             """
         self.init_status()
 
+    async def resolve_replies(self, session: AsyncSession, message: str) -> str:
+        matches = list(PATTERN.finditer(message))
+        if not matches:
+            return message
+
+        for match in reversed(matches):
+            reply_id = int(match.group(1))
+            result = await session.execute(select(Message).where(Message.msg_id == reply_id))
+            row = result.scalars().one_or_none()
+            if row is not None:
+                replacement = f"[CQ:reply,id={reply_id},content={row.msg},from_nickname={row.user_nickname},from_card={row.user_card}]"
+                message = message[: match.start()] + replacement + message[match.end() :]
+
+        return message
+
     @plugin_main(check_call_word=False, check_group=False, require_db=True)
     async def main(self, event: GroupMessageEvent, debug):
         async_sessions = sessionmaker(
@@ -41,10 +62,11 @@ class MessageRecorder(Plugins):
         )
         async with async_sessions() as session:
             async with session.begin():
+                resolved_message = await self.resolve_replies(session, event.message)
                 new_msg = Message(
                     user_id=event.user_id,
                     group_id=event.group_id,
-                    msg=event.message,
+                    msg=resolved_message,
                     msg_id=event.message_id,
                     user_nickname=event.nickname,
                     user_card=event.card,
