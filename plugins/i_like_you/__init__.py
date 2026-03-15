9ca68dd7174831c5095734bc90b08d30d2e3ff47
cojitaZ <1456021165@qq.com>
2026-03-15 18:41:02 +0800

    feat:我喜欢你
    请务必把电棍加入小特

---
+++ plugins/i_like_you/__init__.py
@@ -0,0 +1,1 @@
+from .i_like_you import i_like_you
---
+++ plugins/i_like_you/i_like_you.py
@@ -0,0 +1,41 @@
+from plugins import Plugins, plugin_main
+from src.event_handler import GroupMessageEventHandler
+from src.PrintLog import Log
+from pathlib import Path
+import os
+log = Log()
+
+
+class i_like_you(Plugins):
+    def __init__(self, server_address, bot):
+        super().__init__(server_address, bot)
+        self.name = "i_like_you"
+        self.type = "Group"
+        self.author = "cojitaZ"
+        self.introduction = """
+                                i_like_you
+                                usage: 我喜欢你，你喜欢我
+                            """
+        self.init_status()
+
+    @plugin_main()
+    async def main(self, event: GroupMessageEventHandler, debug):
+        message = event.message
+        if "我喜欢你" in message:
+            try:
+
+                self.api.groupService.send_group_record_msg(group_id=event.group_id,file_path=Path.cwd()/ "plugins" / "i_like_you" / "我喜欢你_你喜欢我.wav")
+
+            except Exception as e:
+                log.error(f"插件{self.name}运行时出错，{e}")
+            else:
+                log.debug(f"成功向{event.group_id}发送我喜欢你", debug)
+
+            return
+        elif "我不喜欢你" in message:
+            try:
+                self.api.groupService.send_group_record_msg(group_id=event.group_id,file_path=Path.cwd()/ "plugins" / "i_like_you" / "我不喜欢你.wav")
+            except Exception as e:
+                log.error(f"插件{self.name}运行时出错，{e}")
+            else:
+                log.debug(f"成功向{event.group_id}发送我不喜欢你", debug)
